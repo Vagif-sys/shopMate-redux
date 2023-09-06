76c3b46f0db645ef7cb1ee0f3354d14115fe596c
Vagif <you@example.com>
2023-09-06 13:37:33 +0400

    Shop Redux

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import { Link, NavLink } from "react-router-dom";
+import {useSelector} from 'react-redux'
+import Logo from "../assets/logo.png"
+import "./Header.css";
+
+export const Header = () => {
+  const countHeader = useSelector(state => state.cartState.cartList)
+  return (
+    <header>
+      <Link to="/" className="logo">
+        <img src={Logo} alt="Shopmate Logo" />
+        <span>Shopping Cart</span>
+      </Link>
+      <nav className="navigation">
+        <NavLink to="/" className="link" end>Home</NavLink>
+        <NavLink to="/cart" className="link">Cart</NavLink>
+      </nav>
+      <Link to="/cart" className="items">
+        <span>Cart: {countHeader.length}</span>
+      </Link>
+    </header>
+  )
+}
